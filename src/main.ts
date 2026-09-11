dd7d1c383e12873b99ea7ae1c263b798c775cfe3
Egor G. <egor@swapzone.io>
2026-09-11 14:12:12 +0300

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,30 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Включаем CORS для всех источников (чтобы локальный HTML файл мог делать запросы)
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  if (process.env.IS_STATIC === 'true') {
+    const rootPublic = join(process.cwd(), 'public');
+    const distPublic = join(__dirname, '..', 'public');
+    
+    // Безопасное определение пути к публичной папке
+    const staticPath = fs.existsSync(rootPublic) ? rootPublic : distPublic;
+    
+    app.useStaticAssets(staticPath);
+    Logger.log(`Раздача статики включена из: ${staticPath}`, 'Bootstrap');
+  }
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
